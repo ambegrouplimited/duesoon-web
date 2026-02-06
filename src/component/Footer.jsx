7dc1d22396322de59f4d04e75f5af8d5774787ca
Rasheed Salami <136803958+CommanderOutpost@users.noreply.github.com>
2026-02-06 11:16:46 +0100

    feat: adds ambe group to footer

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -1,5 +1,5 @@
-import { Link, useLocation } from 'react-router-dom';
-import duesoon from '/src/assets/duesoonlogo.png';
+import { Link, useLocation } from "react-router-dom";
+import duesoon from "/src/assets/duesoonlogo.png";
 
 export default function Footer() {
   const location = useLocation();
@@ -20,12 +20,17 @@ export default function Footer() {
     <footer className="section border-t relative z-20 ">
       <div className="max-w-6xl mx-10 md:mx-auto px-6 py-16 grid grid-cols-2 gap-12 md:grid-cols-4">
         {/* Brand */}
-        <div className='max-w-xs'>
-          
-          <img src={duesoon} alt="" srcset="" className='h-8 w-auto object-contain' />
-          
+        <div className="max-w-xs">
+          <img
+            src={duesoon}
+            alt=""
+            srcset=""
+            className="h-8 w-auto object-contain"
+          />
+
           <p className="mt-4 text-sm text-gray-600">
-            Get paid on time, automatically, without chasing clients or sending awkward reminders.
+            Get paid on time, automatically, without chasing clients or sending
+            awkward reminders.
           </p>
         </div>
 
@@ -75,7 +80,11 @@ export default function Footer() {
         <div>
           <p className="font-bold mb-4">SUPPORT</p>
           <ul className="space-y-2 text-sm text-gray-600">
-            <li><Link to="/contact" className="hover:text-gray-900">Contact</Link></li>
+            <li>
+              <Link to="/contact" className="hover:text-gray-900">
+                Contact
+              </Link>
+            </li>
             <li>
               <Link
                 to="/#faqs"
@@ -91,14 +100,23 @@ export default function Footer() {
         <div>
           <p className="font-bold mb-4">LEGAL</p>
           <ul className="space-y-2 text-sm text-gray-600">
-            <li><Link to="/privacy" className="hover:text-gray-900">Privacy Policy</Link></li>
-            <li><Link to="/terms" className="hover:text-gray-900">Terms of Service</Link></li>
+            <li>
+              <Link to="/privacy" className="hover:text-gray-900">
+                Privacy Policy
+              </Link>
+            </li>
+            <li>
+              <Link to="/terms" className="hover:text-gray-900">
+                Terms of Service
+              </Link>
+            </li>
           </ul>
         </div>
       </div>
 
       <div className="border-t py-6 text-center text-sm text-gray-500">
-        © {new Date().getFullYear()} DueSoon. All rights reserved To DueSoon.
+        © {new Date().getFullYear()} DueSoon. All rights reserved To AMBE Group
+        Limited.
       </div>
     </footer>
   );
